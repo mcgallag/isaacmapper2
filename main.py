5c6d5805704647c39e2a3130ab6d47ba889cb513
Mike Gallagher <mcgallag@gmail.com>
2018-09-03 13:43:32 -0400

    Started UI

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
-import sys, pygame
+import pygame
 pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
-black = 0, 0, 0
+size = width, height = 1024, 768
+black = 200, 200, 200
 
 screen = pygame.display.set_mode(size)
 
 room = pygame.image.load("images/room.png")
 roomrect = room.get_rect()
+roomrect.left = width // 2 - 16
+roomrect.top = height // 2 - 16
 
-while 1:
+running = True
+
+dx = 0
+dy = 0
+
+while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            running = False
+            continue
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                continue
+            elif event.key == pygame.K_DOWN:
+                dy -= 32
+            elif event.key == pygame.K_UP:
+                dy += 32
+            elif event.key == pygame.K_LEFT:
+                dx += 32
+            elif event.key == pygame.K_RIGHT:
+                dx -= 32
 
-    roomrect = roomrect.move(speed)
-
-    if roomrect.left < 0 or roomrect.right > width:
-        speed[0] = -speed[0]
-    if roomrect.top < 0 or roomrect.bottom > height:
-        speed[1] = -speed[1]
+    roomrect = roomrect.move(dx, dy)
+    dx = 0
+    dy = 0
 
     screen.fill(black)
     screen.blit(room, roomrect)
     pygame.display.flip()
+    pygame.time.delay(1000 // 60)
